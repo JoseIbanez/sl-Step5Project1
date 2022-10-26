a118873c481ae02d4c9f842fc460fa7ff07a6453
Jose Ibanez <jose.ibanez@vodafone.com>
2022-10-26 20:26:25 +0200

    first commit

---
+++ website/Readme.txt
@@ -0,0 +1,25 @@
+# Links
+
+https://www.youtube.com/watch?v=yGYeYJpRWPM&ab_channel=SamMeech-Ward
+https://github.com/Sam-Meech-Ward/s3-direct-upload
+
+
+# Prepare
+
+```bash
+cd back
+npm install
+````
+
+# Serve
+
+```bash
+
+export BUCKETNAME=""
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+
+npm back/serve.js
+````
+
+
---
+++ website/back/s3.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv'
+import aws from 'aws-sdk'
+import crypto from 'crypto'
+import { promisify } from "util"
+const randomBytes = promisify(crypto.randomBytes)
+
+dotenv.config()
+
+const region = "us-east-1"
+const bucketName = process.env.BUCKETNAME
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
+
+const s3 = new aws.S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+  signatureVersion: 'v4'
+})
+
+export async function generateUploadURL() {
+  const rawBytes = await randomBytes(16)
+  const imageName = rawBytes.toString('hex')
+
+  console.log("name:"+imageName)
+
+  const params = ({
+    Bucket: bucketName,
+    Key: imageName,
+    Expires: 60
+  })
+  
+  const uploadURL = await s3.getSignedUrlPromise('putObject', params)
+  return uploadURL
+}
