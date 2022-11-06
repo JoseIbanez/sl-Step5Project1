a724a811d0974510228860833d99971220e4b252
Jose Ibanez <jose.ibanez@vodafone.com>
2022-11-06 14:22:02 +0100

    modular mode

--- lambda_del/src/lambda_function.py
+++ lambda_del/src/lambda_function.py
@@ -10,23 +10,43 @@ BUCKET_DEL="sl-ibanez-s5p1-deleted"
 
 def lambda_handler(event, context):
     # TODO implement
-    print("--event--")
-    print(json.dumps(event))
-    
-    sns_msg = event['Records'][0]['Sns']['Message']
-    
-    print("--sns--")
-    print(sns_msg)
-    
-    sns = json.loads(sns_msg)
-    
-    content_b64 = sns['content']
-    print("--content b64--")
-    print(content_b64)
-    
-    content = base64.b64decode(content_b64).decode('utf-8')
-    print("--content--")
-    print(content)
+
+    content = parse_event(event)
+    if not content:
+        return
+
+    filename = scan_mail(content)
+    if not filename:
+        return
+
+    move_file(filename)
+
+
+def parse_event(event):
+
+    try:
+        print("--event--")
+        print(json.dumps(event))
+        
+        sns_msg = event['Records'][0]['Sns']['Message']
+        
+        print("--sns--")
+        print(sns_msg)
+        
+        sns = json.loads(sns_msg)
+        
+        content_b64 = sns['content']
+        print("--content b64--")
+        print(content_b64)
+        
+        content = base64.b64decode(content_b64).decode('utf-8')
+        print("--content--")
+        print(content)
+        return content
+
+    except Exception as e:
+        print(e)
+        return None
 
 
 
@@ -37,7 +57,7 @@ def scan_mail(content):
 
     for line in content.split('\n'):
 
-
+        # Ignore html part
         if "<br>" in line:
             continue
 
@@ -52,6 +72,7 @@ def scan_mail(content):
             print(line)
             delete_command = True
             continue
+
 
         # Detect the filename from original mail
         m = re.search("^filename: (.+)$",line)
@@ -84,3 +105,5 @@ def move_file(filename):
     s3.delete_object(
         Bucket=BUCKET_INC,
         Key=filename)
+
+        
